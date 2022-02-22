dba6f6028ac53e54ed58fd32ae864d34db11c822
Zhenqing Lang <zhenqinglang@cmail.carleton.ca>
2022-02-22 18:54:48 -0500

    update search page

---
+++ src/components/TrendCoinCard.js
@@ -0,0 +1,70 @@
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native'
+import React from 'react'
+import { Card } from 'react-native-elements/dist/card/Card'
+import { Divider } from 'react-native-elements';
+
+const styles = StyleSheet.create({
+    itemWrapper: {
+        flex: 1,
+        paddingHorizontal: 8,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        paddingVertical: 4,
+    },
+    leftWrapper: {
+        flex: 1,
+        flexDirection: "row",
+        alignItems: "center",
+        marginRight: 12
+    },
+    image: {
+        height: 30,
+        width: 30,
+    },
+    rightWrapper: {
+        flex: 1,
+        alignItems: "flex-end"
+    },
+    titleWrapper: {
+        marginLeft: 8,
+    },
+    title: {
+        fontSize: 16
+    },
+    subtitle: {
+        fontSize: 12,
+        marginTop: 4,
+        color: "#A9ABB1"
+    },
+    rankStyle: {
+        fontSize: 12,
+        marginTop: 4,
+        color: "#A9ABB1"
+    }
+
+});
+
+const TrendCoinCard = ({ name, symbol, small, rank }) => {
+    return (
+        <TouchableOpacity style={{ marginHorizontal: 18 }}>
+            <View style={styles.itemWrapper}>
+                {/* LeftSide */}
+                <View style={styles.leftWrapper}>
+                    <Image source={{ uri: small }} style={styles.image} />
+                    <View style={styles.titleWrapper}>
+                        <Text style={styles.title}>{name}</Text>
+                        <Text style={styles.subtitle}>{symbol}</Text>
+                    </View>
+                </View>
+                {/* rightSide */}
+                <View style={styles.rightWrapper}>
+                    <Text style={styles.rankStyle}>#{rank}</Text>
+                </View>
+            </View>
+            <Divider />
+        </TouchableOpacity>
+    )
+}
+
+export default TrendCoinCard
