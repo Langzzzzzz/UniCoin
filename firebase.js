45b9e1bacb07cd145017adc40af9d8eae897a41a
Zhenqing Lang <zhenqinglang@cmail.carleton.ca>
2022-05-27 17:17:26 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -8,13 +8,6 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAGb9bmXaG5JQesjIpr3MAfn05-dmq-AXw",
-  authDomain: "unicoin-6c0b9.firebaseapp.com",
-  projectId: "unicoin-6c0b9",
-  storageBucket: "unicoin-6c0b9.appspot.com",
-  messagingSenderId: "581296756845",
-  appId: "1:581296756845:web:2a214fb2082e815f628764",
-  measurementId: "G-GQ3NLR8MLJ"
 };
 
 // Initialize Firebase
